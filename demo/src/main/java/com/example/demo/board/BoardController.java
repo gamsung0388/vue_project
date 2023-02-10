230bd8107fa401979378a90430e2e3ec0c8f214c
gamsung0388 <82205370+gamsung0388@users.noreply.github.com>
2023-02-10 17:21:42 +0900

    vue 2번째 커밋

--- demo/src/main/java/com/example/demo/board/BoardController.java
+++ demo/src/main/java/com/example/demo/board/BoardController.java
@@ -1,25 +1,50 @@
 package com.example.demo.board;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@CrossOrigin(origins="http://localhost:3030")
 public class BoardController {
 
-	@Autowired
 	private BoardService boardService;
 	
-	public Map<String, Object> boardInsert(BoardDTO boardDTO){
+	@PostMapping("/board/insert")
+	public Map<String, Object> boardInsert(@RequestBody BoardDTO boardDTO){
 		
 		Map<String, Object> map = new HashMap<>();
 		
+		System.out.println("boardDTO: "+boardDTO);
+		
 		String board = boardService.boardInsert(boardDTO);
+		
+		System.out.println("등록 성공");
 		
 		map.put("YN", board);
 				
 		return map;
 	}
+	
+	@GetMapping("/board/list")
+	public List<BoardDTO> boardList(){
+		
+		List<BoardDTO> list = boardService.boardSelect();
+		
+		System.out.println("selectList: "+list);
+		
+		return list;
+		
+	}
+	
+	
 }
---
+++ demo/src/test/java/com/example/demo/BoardTest.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.board.BoardDTO;
+import com.example.demo.board.BoardService;
+
+@SpringBootTest
+public class BoardTest {
+
+	@Autowired
+	private BoardService boardService;
+	
+	@Test
+	public void boardInsert() {
+		
+		BoardDTO boardData = new BoardDTO();
+		
+		boardData.setBoardTitle("테스트 제목");
+		boardData.setBoardTxt("테스트 내용");
+		boardData.setBoardTag("테스트 태그");
+		boardData.setUserId("test1234");
+		
+		boardService.boardInsert(boardData);
+			
+	}
+	
+	@Test
+	public void boardUpdate() {
+		
+		BoardDTO boardData = new BoardDTO();
+		
+		boardData.setBoardTitle("테스트 제목");
+		boardData.setBoardTxt("테스트 내용");
+		boardData.setBoardTag("테스트 태그");
+		
+		
+		boardService.boardUpdate(boardData);
+	}
+	
+	@Test
+	public void boardDelete() {
+		
+		int bnum = 1;
+		
+		boardService.boardDelete(bnum);
+		
+	}
+	@Test
+	public void list(){
+		
+		List<BoardDTO> list = boardService.boardSelect();
+			
+		System.out.println("list: "+list);
+	
+	}
+	
+	
+}
