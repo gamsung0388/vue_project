787946c2f6aff8fe5fcfa15e961cddd248ad9c8a
gamsung0388 <82205370+gamsung0388@users.noreply.github.com>
2023-03-10 15:59:05 +0900

    로그인 초기 추가

---
+++ demo/src/main/java/com/example/demo/login/LoginMapper.java
@@ -0,0 +1,8 @@
+package com.example.demo.login;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class LoginMapper {
+
+}
---
+++ demo/src/main/java/com/example/demo/login/LoginService.java
@@ -0,0 +1,8 @@
+package com.example.demo.login;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+}
